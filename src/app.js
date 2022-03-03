ed9be1324ff19af60c2a3b4a5613baba53c2c2c4
Ldbshear-jones <ldbshearjones@gmail.com>
2022-03-02 22:31:05 -0500

    styling/functionality
    added more css styling and javascript to support user interactions

--- src/app.js
+++ src/app.js
@@ -27,3 +27,46 @@ function dayClock() {
   const dayNTime = document.getElementById("dayTime");
   dayNTime.textContent = `${weekDay} ${hour}:${minutes}`;
 }
+
+let searchForm = document.getElementById("searchForm");
+let searchIcon = document.getElementById("submitSearch");
+
+function showWeather(response) {
+  console.log(response);
+  document.getElementById("hiTemp").innerHTML = `${response.data.main.temp}`;
+  let currentIcon = response.data.weather[0].icon;
+  document.getElementById(
+    "currentWeatherIcon"
+  ).innerHTML = `<img src="images/icons/${currentIcon}.png" class="lgIcon" />`;
+  document.querySelector(
+    "#currentWeatherDetails"
+  ).innerHTML = `${response.data.weather[0].description}`;
+  document.querySelector(
+    "#wind"
+  ).innerHTML = `Windspeed: ${response.data.wind.speed}`;
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity:${response.data.main.humidity}`;
+}
+
+//Stops normal submit and refresh behavior
+function getCity(event) {
+  event.preventDefault();
+  let userEntry = document.querySelector("#userCity").value;
+  document.getElementById("updateCity").innerHTML = `${userEntry}`;
+  requestCity(userEntry);
+}
+
+function requestCity(userEntry) {
+  console.log(userEntry);
+  let devKey = "3711439e85a5b0487eab981ef384735a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userEntry}&appid=${devKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function activate() {
+  document.querySelector("form").requestSubmit();
+}
+
+searchIcon.addEventListener("click", activate);
+searchForm.addEventListener("submit", getCity);
