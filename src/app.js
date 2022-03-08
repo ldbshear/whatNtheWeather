b96b08bc53fa367069d1f1ae3f09ba56735c6409
Ldbshear-jones <ldbshearjones@gmail.com>
2022-03-08 16:57:08 -0500

    html/css/javascript updates
    changed background of app. reduced size of paw print. changed html layout in favor of flexbox. working on js updates

--- src/app.js
+++ src/app.js
@@ -30,43 +30,42 @@ function dayClock() {
 
 let searchForm = document.getElementById("searchForm");
 let searchIcon = document.getElementById("submitSearch");
-
-function test(response) {
-  console.log(response);
-}
+let metric = document.getElementById("celsius");
 
 function showWeather(response) {
   console.log(response);
   document.getElementById("updateCity").innerHTML = `${response.data.name}`;
   document.getElementById("hiTemp").innerHTML = `${response.data.main.temp}`;
   let currentIcon = response.data.weather[0].icon;
+  let weatherDescription = response.data.weather[0].description;
+  let feelsLike = response.data.main.feels_like;
+
   document.getElementById(
     "currentWeatherIcon"
   ).innerHTML = `<img src="images/icons/${currentIcon}.png" class="lgIcon" />`;
   document.querySelector(
     "#currentWeatherDetails"
-  ).innerHTML = `${response.data.weather[0].description}`;
+  ).innerHTML = `${weatherDescription.toUpperCase()} in ${response.data.name}.`;
   document.querySelector(
     "#wind"
-  ).innerHTML = `Windspeed: ${response.data.wind.speed}`;
+  ).innerHTML = `There is currently a windspeed of ${response.data.wind.speed}`;
   document.querySelector(
     "#humidity"
-  ).innerHTML = `Humidity:${response.data.main.humidity}`;
+  ).innerHTML = `With a humidity of ${response.data.main.humidity}%.`;
+  document.querySelector(
+    "#feelsLike"
+  ).innerHTML = `The temperature feels like it's ${feelsLike}° outside.`;
 }
 
 //Stops normal submit and refresh behavior
-function getCity(event) {
-  event.preventDefault();
-  let userEntry = document.querySelector("#userCity").value;
-  document.getElementById("updateCity").innerHTML = `${userEntry}`;
-  requestCity(userEntry);
-}
 
 function defaultWeather() {
   let devKey = "3711439e85a5b0487eab981ef384735a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&appid=${devKey}&units=imperial`;
+
   axios.get(apiUrl).then(showWeather);
 }
+
 
 function requestCity(userEntry) {
   console.log(userEntry);
@@ -75,9 +74,18 @@ function requestCity(userEntry) {
   axios.get(apiUrl).then(showWeather);
 }
 
+function getCity(event) {
+  event.preventDefault();
+  let userEntry = document.querySelector("#userCity").value;
+  document.getElementById("updateCity").innerHTML = `${userEntry}`;
+  requestCity(userEntry);
+}
+
 function activate() {
   document.querySelector("form").requestSubmit();
 }
+
+//metric.addEventListener("click", showMetric);
 
 searchIcon.addEventListener("click", activate);
 searchForm.addEventListener("submit", getCity);
