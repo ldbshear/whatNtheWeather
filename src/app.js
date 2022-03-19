a3daafe9c259b77cd1783be4ad3c7cdb2d6c70f6
Ldbshear-jones <ldbshearjones@gmail.com>
2022-03-18 20:41:22 -0400

    project reqirements
    added html template and api call for 5 day weather

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@ function dayClock() {
   dayNTime.textContent = `${weekDay} ${hour}:${minutes}`;
 }
 
+let forecastBtn = document.getElementById("fiveDayBtn");
 let locator = document.getElementById("geoHouse");
 let imperial = document.getElementById("farenheit");
 let metric = document.getElementById("celsius");
@@ -54,7 +55,6 @@ const getWeather = {
   },
 
   showWeather: function (response) {
-    console.log(response);
     document.getElementById("updateCity").innerHTML = `${response.data.name}`;
     document.getElementById("hiTemp").innerHTML = `${response.data.main.temp}`;
     let currentIcon = response.data.weather[0].icon;
@@ -78,6 +78,34 @@ const getWeather = {
     document.querySelector(
       "#feelsLike"
     ).innerHTML = `The temperature feels like it's ${feelsLike}° outside.`;
+  },
+
+  test: function (userEntry) {
+    console.log("button clicked 1");
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${userEntry}&appid=${this.devKey}&units=imperial`;
+    axios.get(apiURL).then(this.getForecast);
+  },
+
+  getForecast: function (res) {
+    console.log("button clicked 2");
+    let lat = res.data.coord.lat;
+    let lon = res.data.coord.lon;
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts,current&appid=${getWeather.devKey}&units=imperial`;
+    axios.get(apiURL).then(getWeather.showForecast);
+  },
+
+  showForecast: function (res) {
+    console.log(res);
+    const userCardTemplate = document.querySelector("[data-user-template]");
+
+    const userCardContainer = document.querySelector(
+      "[data-user-cards-container]"
+    );
+    userCardContainer.innerHTML = "";
+    const card = userCardTemplate.content.cloneNode(true).children[0];
+    const weatherForecast = card.querySelector("[data-body");
+    weatherForecast.textContent = res.data.daily[0].temp.max;
+    userCardContainer.append(card);
   },
 };
 
@@ -105,6 +133,11 @@ function getCity(event) {
 function activate() {
   document.querySelector("form").requestSubmit();
 }
+
+forecastBtn.addEventListener("click", function () {
+  let userEntry = document.querySelector("#userCity").value;
+  getWeather.test(userEntry);
+});
 
 locator.addEventListener("click", navigate);
 
